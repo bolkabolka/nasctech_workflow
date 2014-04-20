6b71d19e8035fabed5f143873a9d83ceefac7f7d
Slavik Balabolka <bolkabolka@gmail.com>
2014-04-21 02:13:22 +0300

    parse git branch

--- lib/nasctech_workflow/git.rb
+++ lib/nasctech_workflow/git.rb
@@ -1,12 +1,15 @@
 class NasctechWorkflow::Git
   class << self
+    def branch_name
+      `git rev-parse --abbrev-ref HEAD`.strip
+    end
+
     def last_message
-      `git log -1 --oneline`
+      `git log -1 --oneline`.strip
     end
 
     def parse_ticket_number
-      msg = last_message.strip
-      $1.to_i if msg =~ /\s#(\d{4}):/
+      $1.to_i if last_message =~ /\s#(\d{4}):/
     end
   end
 end

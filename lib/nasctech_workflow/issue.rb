7e0b17f7ace56ee07dcdd6c635e8cee85c6206ba
Slavik Balabolka <bolkabolka@gmail.com>
2014-04-22 12:37:30 +0300

    select reason for bug

--- lib/nasctech_workflow/issue.rb
+++ lib/nasctech_workflow/issue.rb
@@ -15,6 +15,15 @@ module NasctechWorkflow
       feedback_sup: 17,
       escalation: 19
     }
+    BUG_REASONS = ['Merge', 'Spelling Typo', 'Imp', 'Wrong algorithm',
+                   'Impact of other commit', 'Wrong use case', 'Wrong setting/data',
+                   'Environment problems', 'Mismatch design', 'Wrong Design',
+                   'Wrong Arch', 'Lib issue', 'Code typo']
+    TYPES_REQUIRE_REASON = {
+      bug: 1,
+      hotfix: 5
+    }
+    REASON = 37
     PARENT_BRANCH = 16
     GUI_BRANCH = 14
     SERVER_BRANCH = 22
@@ -39,12 +48,14 @@ module NasctechWorkflow
     end
 
     def set_branches
-      self.custom_field_values = {}
+      self.custom_field_values ||= {}
       self.custom_field_values[GUI_BRANCH.to_s] = parent_branch
       self.custom_field_values[SERVER_BRANCH.to_s] = parent_branch
     end
 
     def progress!(branch: nil, duration: nil)
+      self.custom_field_values ||= {}
+
       if !branch && gui_branch_not_set?
         branch = NasctechWorkflow::Git.detect_branch_name
       end
@@ -71,6 +82,12 @@ module NasctechWorkflow
     end
 
     def review!(branch: nil, duration: nil)
+      self.custom_field_values ||= {}
+
+      if TYPES_REQUIRE_REASON.values.include? tracker.id
+        self.custom_field_values[REASON.to_s] = select_reason
+      end
+
       NasctechWorkflow::TimeEntry.log_development(id, duration) if duration
 
       self.custom_field_values[GUI_BRANCH.to_s] = branch
@@ -78,6 +95,15 @@ module NasctechWorkflow
       self.assigned_to_id = 41
       self.status_id = STATUSES[:review]
       save
+    end
+
+    def select_reason
+      choose do |menu|
+        menu.prompt = 'Select bug reason: '
+        menu.choices(*BUG_REASONS) do |reason|
+          return reason
+        end
+      end
     end
   end
 end
